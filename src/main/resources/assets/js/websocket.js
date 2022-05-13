e58df5b2f5358a055af3c3b40211562e59322523
unknown <birolkarmaker@gmail.com>
2022-05-14 00:00:04 +0600

    from home minor

--- src/main/resources/assets/js/websocket.js
+++ src/main/resources/assets/js/websocket.js
@@ -70,8 +70,10 @@ function showGreeting(senderid,sender,message,time,rcvr,rcvrid,color) {
 			+ "</div>" 
 			+ "</div>" + "</div>" + "</div>";
 	    */
-	$content="<tr><td class='text-muted'>"+time+"</td>" +
-			"<td class='text-justify'><a href='#' emp-id='"+senderid+"' onclick='viewEmlpoyee(this);'  style='color: "+color+"'>"+sender+"</a>: "+message+"</td></tr>";
+	$content = "<tr>" + "<td class='text-justify'><a href='#' emp-id='"
+			+ senderid + "' onclick='viewEmlpoyee(this);'  style='color: "
+			+ color + "'>" + sender + "</a>: " + message + "</td>" +
+			"<td class='text-muted col-sm-3 text-center'>" + time + "</td></tr>";
 	
 	
     //alert($content);
