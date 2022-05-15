44da09a5df4659092e76fdcded358770f21de6bc
Bindu Birol <33890858+BinduBirol@users.noreply.github.com>
2022-05-15 22:28:50 +0600

    bindu home

--- src/main/java/com/birol/ems/dto/SendMessage.java
+++ src/main/java/com/birol/ems/dto/SendMessage.java
@@ -7,9 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class SendMessage {
 	private String content;
 	private String sender;
